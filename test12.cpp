261420fe272b2456f13aa491e51d0b554572258f
Foolary <aryanraghoenath@gmail.com>
2026-01-26 13:14:20 -0300

    Refactor main function and clean up code

--- test12.cpp
+++ test12.cpp
@@ -27,11 +27,24 @@ int main(){
     while(name.empty());
         //cin >> name;
        cout << "Hello " << name;
+/* //int i= 0;
+for (  int i= 0;i <= 50; i+=5 ){
+  cout << i << '\n';
+  if (i == 6){cout <<"lol 6 HEHEHE "<< '\n';}
+else if (i==7){cout <<"lol 7 FUKC I SAID IT"<< '\n';}
+else {cout << "RAAAAAWWRR"<<'\n';}*/
 
+   //for(int i = 10; i > 0; i--){
+      //count down to 10
+     // std::cout << i << '\n';
+//cout << x;
+ // do{cout << "lol" << '\n';}
+ //if(x == 6 ){cout << "lol" << '\n';}
     
    
   
 
     return 0;
+
 
 }
